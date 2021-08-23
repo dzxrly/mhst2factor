06b058bedd7ad4ad4ef7fbb7159e780587d72401
EggTargaryen <dzxrly2@gmail.com>
2021-08-23 20:22:00 +0800

    :bookmark: v1.0.3 release: 更新了因子和技能数据

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -26,7 +26,7 @@ import {defineComponent, ref} from 'vue'
 export default defineComponent({
   name: 'MainLayout',
   setup() {
-    const version = ref('v1.0.2')
+    const version = ref('v1.0.3')
 
     return {
       version
